ff1c2d34bd28a80f355f4c46c07f3ace430efd32
Moalmortagel <105941198+Almortage@users.noreply.github.com>
2024-06-12 04:02:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-OWNER_ID = os.environ.get("OWNER_ID", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    OWNER_ID = os.environ.get("OWNER_ID", "")
