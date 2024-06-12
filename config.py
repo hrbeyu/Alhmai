d2164d7d4a3da8f78de0e6eec6515b976a3ebec0
Moalmortagel <105941198+Almortage@users.noreply.github.com>
2024-06-12 03:58:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,6 @@
-import re
-import sys
-from os import getenv
-
-from dotenv import load_dotenv
-from pyrogram import filters
-
-load_dotenv()
+import os
 
 
-
-
-BOT_TOKEN = getenv("BOT_TOKEN")
-
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Almortagel")
-
-OWNER_ID = list(
-  map(int, getenv("OWNER_ID", "").split())) + [5089553588]
-USER_OWNER = getenv("USER_OWNER","Almortagel_12") 
+class Config(object):
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+OWNER_ID = os.environ.get("OWNER_ID", "")
