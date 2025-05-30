d19be573f3135340e9abf8d1ecc64e49caed002b
hrbeyu <hwusuay@gmail.com>
2025-05-30 10:23:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    OWNER_ID = os.environ.get("OWNER_ID", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7839574004:AAHLKnWsfvRfg42CH3WFbr9t2JS1dmrz8dc") 
+    OWNER_ID = os.environ.get("OWNER_ID", "7182427468")
