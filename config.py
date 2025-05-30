be0a1ab649461ff5cb1806cbbe281cd4c48f6d76
hrbeyu <hwusuay@gmail.com>
2025-05-30 10:28:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7839574004:AAHLKnWsfvRfg42CH3WFbr9t2JS1dmrz8dc") 
-    OWNER_ID = os.environ.get("OWNER_ID", "7182427468")
+    BOT_TOKEN = "7839574004:AAHLKnWsfvRfg42CH3WFbr9t2JS1dmrz8dc"
+OWNER_ID = 7182427468  # ضع هنا رقم الـ ID الخاص بك أو بصاحب البوت
